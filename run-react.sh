6b6ac4a726d4daef76ad583f113da5c6913a7a3c
Xavier Yuhan Liu <xavieryuhanliu@outlook.com>
2024-08-15 16:40:10 +0800

    吧声音变成阻塞的了

--- run-react.sh
+++ run-react.sh
@@ -1,3 +1,2 @@
 cd storyparkreact
-#HTTPS=true SSL_CRT_FILE=ca.crt SSL_KEY_FILE=ca.key 
-nohup npm start > nohup-react.out 2>&1 &
+HTTPS=true SSL_CRT_FILE=ca.crt SSL_KEY_FILE=ca.key nohup npm start > nohup-react.out 2>&1 &
--- storyparkserver/app.py
+++ storyparkserver/app.py
@@ -249,6 +249,6 @@ def test_get_audio():
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5000, debug=True)
+  app.run(host='0.0.0.0', port=8000, debug=True)
 
 
