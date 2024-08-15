f1579e63a58de775057c46e63c49456049187d17
Xavier Yuhan Liu <xavieryuhaliu@outlook.com>
2024-08-15 10:57:56 +0800

    update run-react to support real https

--- run-react.sh
+++ run-react.sh
@@ -1,2 +1,3 @@
 cd storyparkreact
-HTTPS=true SSL_CRT_FILE=ca.crt SSL_KEY_FILE=ca.key nohup npm start > nohup-react.out 2>&1 &
+#HTTPS=true SSL_CRT_FILE=ca.crt SSL_KEY_FILE=ca.key 
+nohup npm start > nohup-react.out 2>&1 &
